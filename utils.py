ddb634390718b799589ac037aa257c916f57eab6
Soufiane <souflam007@yahoo.fr>
2019-06-09 18:10:08 +0200

    wo model

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import requests
+import urllib 
 
 def count_words_at_url(url):
     print('counting words')
     resp = requests.get(url)
     print('counting ok')
+    
+    url = "https://dl.fbaipublicfiles.com/XLM/mlm_tlm_xnli15_1024.pth"
+    urllib.request.urlretrieve(url, "mlm_tlm_xnli15_1024.pth")
+ 
+    print('file dwnld ok')
     return len(resp.text.split())
