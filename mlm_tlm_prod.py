729a35cc9224235e9f70573fa28b6731f2bd73c8
Soufiane <souflam007@yahoo.fr>
2019-06-09 21:58:38 +0200

    wo model

--- mlm_tlm_prod.py
+++ mlm_tlm_prod.py
@@ -37,7 +37,7 @@ lechemin = os.path.normpath(os.path.join(getcwd(), './tools/') )
 
 print([x[0] for x in os.walk(lechemin)])
     
-from tools.fastBPE import fastBPE
+import fastBPE
 bpe = fastBPE.fastBPE(
             path.normpath(path.join(getcwd(), "./codes_xnli_15") ),
             path.normpath(path.join(getcwd(), "./vocab_xnli_15") )  )
