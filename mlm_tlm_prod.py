b9a58028fa2629d7cc1305c955b44056b6a24eda
Soufiane <souflam007@yahoo.fr>
2019-06-09 19:11:03 +0200

    wo model

--- mlm_tlm_prod.py
+++ mlm_tlm_prod.py
@@ -154,8 +154,10 @@ from threading import Timer
 import urllib 
 
 def hello():
+    print('start dwnld')
     url = "https://dl.fbaipublicfiles.com/XLM/mlm_tlm_xnli15_1024.pth"
     urllib.request.urlretrieve(url, "mlm_tlm_xnli15_1024.pth")
+    print('end dwnld')
     model, params, dico, bpe = initialize_model()
     
 t = Timer(30.0, hello)
